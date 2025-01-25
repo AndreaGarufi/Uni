0d6c9c7dfe44e1d8bdfa4dadcf396164db8e8989
Andrea Garufi <andreagarufi17@gmail.com>
2025-01-25 11:57:26 +0100

    vault backup: 2025-01-25 11:57:26

--- Primo anno/architettura-degli-elaboratori/Laboratorio/Assembly/2.4-IstruzioniMacchina/esercizio3.s
+++ Primo anno/architettura-degli-elaboratori/Laboratorio/Assembly/2.4-IstruzioniMacchina/esercizio3.s
@@ -13,15 +13,15 @@ ciclo     subs    r2,r2,#1
           blt     esci
           ldr     r4,[r1],#4
           ;verifico se è pari
-          and     r5,r4,#1
-          cmp     r5,#0
-          beq     ciclo
-          cmp     r4,#5
-          ble     ciclo
-          add     r3,r3,#1
-          b       ciclo
+          and     r5,r4,#1  ;confronterà il bit meno significativo con 1 e se il risultato è 1 il numero è dispari altrimenti pari
+          cmp     r5,#0 ;compara r5 (risultato dell'and) con 0 
+          beq     ciclo ;salta se il risultato precedente è 0 (cmp r5,#0) (in pratica saltera solo se il numero è pari (perché a me servono i numeri dispari))
+          cmp     r4,#5 ;arriva qui se il numero è dispari e lo compara con 5 per vedere se è maggiore oppure no
+          ble     ciclo ;se è minore di 5 salta a ciclo e va avanti con i numeri
+          add     r3,r3,#1  ;se è maggiore di 5 non avrà saltato e quindi incremento r3 per contare i numeri
+          b       ciclo ;salto a ciclo per continuare con i numeri
 esci      mov     r1,#risultato
-          str     r3,[r1]
+          str     r3,[r1]   ;salvo r3
 
 
 
