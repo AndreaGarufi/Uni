e90618d45edec37f8b2a031086dca0299a85ebef
Andrea Garufi <andreagarufi17@gmail.com>
2025-03-14 19:51:19 +0100

    vault backup: 2025-03-14 19:51:19

--- Primo anno/Secondo Semestre/Programmazione-2/c++/other/memoriaDinamica.cpp
+++ Primo anno/Secondo Semestre/Programmazione-2/c++/other/memoriaDinamica.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main(){
-    
+
+    int *arrayDinamico = new int[10];
+
+    for(int i = 0; i < 10; i++){
+        arrayDinamico[i] = 1;
+        cout << " " <<arrayDinamico[i];
+    }
+
+    delete [] arrayDinamico;
 }
