428d598e2771fdf075bf82865aeac2fb58eca329
Andrea Garufi <andreagarufi17@gmail.com>
2025-05-19 18:08:51 +0200

    vault backup: 2025-05-19 18:08:51

--- Primo anno/Secondo Semestre/Programmazione-2/c++/SpataLab/pilaSPATA.cpp
+++ Primo anno/Secondo Semestre/Programmazione-2/c++/SpataLab/pilaSPATA.cpp
@@ -1,7 +1,6 @@
 /*
     Scrivere una funzione copia pila che copia il contenuto di una pila in un'altra
     La funzione deve avere due argomenti di tipo pila, uno per la pila sorgente e un altro per la pila destinazione
-    SPIEGAZIONE: uso una pila d'appoggio per la copia temporanea e poi la uso per riempire di nuovo le due pile
 */
 
 #include <iostream>
@@ -21,10 +20,15 @@ class Node{
 
 class Stack{
     public:
-    Stack():head(nullptr){}
+    Stack():head(nullptr),lun(0){}
     void push(int d);
     void pop();
     void print();
+    int getLun(){
+        return lun;
+    }
+    void creaPila2(int dim);
+    void copiaPila(Stack& pila1, Stack& pila2);
 
     private:
     Node *head;
@@ -59,6 +63,30 @@ void Stack::print(){
 
 }
 
+void Stack::creaPila2(int dim){
+
+    for(int i = 0; i < dim; i++){
+        head = new Node(0,head);
+    }
+    return;
+
+}
+
+void Stack::copiaPila(Stack& pila1, Stack& pila2){
+
+    Node *currentPila1 = pila1.head;
+    Node *currentPila2 = pila2.head;
+
+    while(currentPila1 != nullptr){
+
+        currentPila2->dato = currentPila1->dato;
+
+        currentPila2 = currentPila2->next;
+        currentPila1 = currentPila1->next;
+    }
+    
+}
+
 
 int main(){
     Stack pila;
@@ -81,7 +109,17 @@ int main(){
     pila.push(1);
     pila.print();
 
-    
+    cout << endl <<"Copio la pila" <<endl;
+
+    Stack pila2;
+    int dim = pila.getLun();
+    pila2.creaPila2(dim);
+    pila2.copiaPila(pila,pila2);
+
+    cout << "Stampo le 2 pile:" << endl << "Pila1 -> ";
+    pila.print();
+    cout << "Pila2 -> ";
+    pila2.print();
 
     return 0;
 }
