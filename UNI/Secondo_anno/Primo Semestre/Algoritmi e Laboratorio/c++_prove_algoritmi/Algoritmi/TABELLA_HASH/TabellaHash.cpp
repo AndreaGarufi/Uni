c263081f5321281ada2f812808f5624d75d90c15
Andrea <andreagarufi17@gmail.com>
2026-02-09 14:57:19 +0100

    vault backup: 2026-02-09 14:57:19

---
+++ UNI/Secondo_anno/Primo Semestre/Algoritmi e Laboratorio/c++_prove_algoritmi/Algoritmi/TABELLA_HASH/TabellaHash.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class Hash{
+
+    public:
+    Hash():array{0},dim(10){}
+
+    private:
+    int array[10];
+    int dim;
+
+};
+
+int main(){
+
+
+}
