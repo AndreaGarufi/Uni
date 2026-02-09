f47c35966047b819f92ad5a0310fb5690d02eb64
Andrea <andreagarufi17@gmail.com>
2026-02-09 21:07:40 +0100

    vault backup: 2026-02-09 21:07:40

--- UNI/Secondo_anno/Primo Semestre/Algoritmi e Laboratorio/c++_prove_algoritmi/Algoritmi/TABELLA_HASH/TabellaHash.cpp
+++ UNI/Secondo_anno/Primo Semestre/Algoritmi e Laboratorio/c++_prove_algoritmi/Algoritmi/TABELLA_HASH/TabellaHash.cpp
@@ -2,18 +2,118 @@
 
 using namespace std;
 
+class List;
+class Node;
+class Hash;
+
+class Node{
+    public:
+    Node(int numero, Node *p = nullptr):next(p),elemento(numero){}
+    friend class List;
+
+    private:
+    int elemento;
+
+    Node *next;
+};
+
+class List{
+    public:
+    List():head(nullptr){}
+    void insert(int numero);
+    void printList();
+
+    ~List(){}
+
+    private:
+    Node *head;
+};
+
 class Hash{
 
     public:
-    Hash():array{0},dim(10){}
+    Hash():array{NULL},dim(10),carico(0),numeroElementi(0){
+
+        for(int i = 0; i < dim; i++){
+            array[i] = new List();
+        }
+    }
+    friend class List;
+    friend class Node;
+    void loadFactor();
+    int hash(int k,int dim);
+    void insertHASH(int k);
+    void printHASH();
 
     private:
-    int array[10];
+    List *array[10];
     int dim;
+    float carico;
+    int numeroElementi;
 
 };
 
+void List::printList(){
+    int i = 0;
+    Node *current = head;
+    while(current != nullptr){
+        cout << current->elemento <<endl;
+        current = current->next;
+        i++;    //contatore lista se è più di 1 allora ci sono collisioni
+        cout << "------"<<i<<"------" <<endl;
+    }
+}
+
+void List::insert(int numero){
+    head = new Node(numero,head);
+    
+}
+
+void Hash::loadFactor(){
+    carico = (float) numeroElementi/dim;
+    float percentuale = carico*100;
+    cout << "Fattore di carico: " << percentuale <<"% " << "Dimensione Totale: " << dim << " + liste concatenate" <<endl;
+}
+
+int Hash::hash(int k,int dim){  //hashing per divisione
+
+    int posizione = k%dim;
+
+    return posizione;
+} 
+
+void Hash::insertHASH(int k){
+    int indice = hash(k,dim);
+    array[indice]->insert(k);
+    numeroElementi++;
+}
+
+void Hash::printHASH(){
+
+    for(int i = 0; i < dim; i++){
+
+        array[i]->printList();
+    }
+}
+
+
 int main(){
+
+    List lista;
+    /*lista.insert(10);
+    lista.insert(5);
+    lista.printList();*/
+
+    Hash tabella;
+
+    tabella.insertHASH(10);
+    tabella.insertHASH(9);
+    tabella.insertHASH(81);
+    tabella.insertHASH(23);
+    tabella.printHASH();
+    tabella.loadFactor();
+
+    //capire se ci sono collisioni e stamparle,
 
 
 }
