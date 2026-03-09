2c53b13dbf7fafa6588c40bffa3e35885dbae02f
Andrea <andreagarufi17@gmail.com>
2026-03-09 14:57:53 +0100

    vault backup: 2026-03-09 14:57:53

--- UNI/Secondo_anno/Secondo Semestre/IngegneriaDelSoftware/java/HelloWorldLEZIONE.java
+++ UNI/Secondo_anno/Secondo Semestre/IngegneriaDelSoftware/java/HelloWorldLEZIONE.java
@@ -1,16 +1,34 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloWorldLEZIONE {
     private static int valore = 0;
     private int contatore;
     private String saluto="Ciao";
     private static LocalDate data= LocalDate.now();
+    private List<String> lista;
 
     public static void main(String[] args){
         System.out.println("Hello World.");
 
-        HelloWorldLEZIONE h=new HelloWorldLEZIONE();
+        HelloWorldLEZIONE h = new HelloWorldLEZIONE();
         h.stampe();
+        h.aggiorna();
+    }
+
+
+    private void aggiorna(){
+
+        System.out.println("LISTA CON ARRAY LIST");
+        lista = new ArrayList<>();
+        lista.add("ciao");
+        lista.add("bye bye");
+
+        for(int i = 0; i < lista.size(); i++){
+            String s = lista.get(i);
+            System.out.println(s);
+        }
     }
 
     private void stampe(){
